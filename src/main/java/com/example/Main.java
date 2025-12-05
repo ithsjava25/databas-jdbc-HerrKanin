0ddeafde5f570f44981e40b731fbd8e424aeaf2b
Edvin Karlsson <edvinkarlsson4@gmail.com>
2025-12-05 11:02:03 +0100

    Skapat metoder login, mainMenuLoop, printMenu, listMoonMissions och getMissionById

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
 
@@ -26,11 +27,43 @@ public class Main {
                             "as system properties (-Dkey=value) or environment variables.");
         }
 
-        try (Connection connection = DriverManager.getConnection(jdbcUrl, dbUser, dbPass)) {
+        try (Connection connection = DriverManager.getConnection(jdbcUrl, dbUser, dbPass);
+             Scanner scanner = new Scanner(System.in)
+        ) {
+            boolean loggedIn = login(scanner,connection);
+
+            if (!loggedIn) {
+                System.out.println("Invalid login");
+                return;
+            }
+
+            mainMenuLoop(scanner, connection);
+
+
+
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
         //Todo: Starting point for your code
+    }
+    private boolean login(Scanner scanner, Connection connection) throws SQLException {
+        System.out.print("Username: ");
+        String username = scanner.nextLine();
+
+        System.out.print("Password: ");
+        String password = scanner.nextLine();
+
+        String sql = "select * from account where name = ? and password = ?";
+
+        try (var ps = connection.prepareStatement(sql)) {
+            ps.setString(1, username);
+            ps.setString(2, password);
+
+            try (var rs = ps.executeQuery()) {
+                return rs.next();
+            }
+        }
+
     }
 
     /**
@@ -58,5 +91,84 @@ public class Main {
             v = System.getenv(envKey);
         }
         return (v == null || v.trim().isEmpty()) ? null : v.trim();
+    }
+
+    private void mainMenuLoop(Scanner scanner, Connection connection) throws SQLException {
+        boolean running  = true;
+        while (running) {
+            printMenu();
+
+            String choice = scanner.nextLine().trim();
+
+            switch (choice) {
+                case "0" -> running = false;
+                case "1" -> listMoonMissions(connection);
+                case "2" -> getMissionById(scanner, connection);
+
+
+                    default -> System.out.println("Invalid option");
+            }
+        }
+    }
+    private void printMenu() {
+        System.out.println("1) List moon missions");
+        System.out.println("2) Get a moon mission by mission:id");
+        System.out.println("3) Count missions for a given year");
+        System.out.println("4) Create an account");
+        System.out.println("5) Update an account");
+        System.out.println("6) Delete an account");
+        System.out.println("0) Exit");
+        System.out.print("Choose: ");
+    }
+    private void listMoonMissions(Connection connection) throws SQLException {
+        String sql = "select spacecraft from moon_mission";
+
+        try (var ps = connection.prepareStatement(sql);
+            var rs = ps.executeQuery()) {
+
+            while (rs.next()) {
+                String spacecraft = rs.getString("spacecraft");
+                System.out.println(spacecraft);
+            }
+        }
+    }
+    private void getMissionById(Scanner scanner, Connection connection) throws SQLException {
+        System.out.print("mission id: ");
+        String input = scanner.nextLine();
+
+        int id;
+        try {
+            id = Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid id");
+            return;
+        }
+
+        String sql = "select * from moon_mission where mission_id = ?";
+
+        try (var ps = connection.prepareStatement(sql)) {
+            ps.setInt(1, id);
+
+            try (var rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    String spacecraft = rs.getString("spacecraft");
+                    String launchDate = rs.getString("launch_date");
+                    String rocket = rs.getString("carrier_rocket");
+                    String operator = rs.getString("operator");
+                    String type = rs.getString("mission_type");
+                    String outcome = rs.getString("outcome");
+
+                    System.out.println("Spacecraft: " +  spacecraft);
+                    System.out.println("Launch date: " + launchDate);
+                    System.out.println("Carrier rocket: " + rocket);
+                    System.out.println("Operator: " + operator);
+                    System.out.println("Mission type: " + type);
+                    System.out.println("Outcome: " + outcome);
+
+                } else {
+                    System.out.println("No mission found");
+                }
+            }
+        }
     }
 }
